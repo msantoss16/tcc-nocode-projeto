6c7c62ac6d2318e3a42d6411f5e4d5b0ab139f92
lucasilmor <77138273+lucasilmor@users.noreply.github.com>
2021-03-20 03:42:48 -0300

    token final

--- api/src/index.js
+++ api/src/index.js
@@ -7,5 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controllers/authentication')(app);
+require('./controllers/projectController')(app);
+
 
 app.listen(3000);

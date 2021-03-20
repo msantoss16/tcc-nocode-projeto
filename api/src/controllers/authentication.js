5425c30e4ff6ca88c54ae48afd1f79acf4c0f69d
lucasilmor <77138273+lucasilmor@users.noreply.github.com>
2021-03-20 02:59:02 -0300

    token login

--- api/src/controllers/authentication.js
+++ api/src/controllers/authentication.js
@@ -8,9 +8,16 @@ const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
 
-
-
 const router = express.Router();
+
+
+function generateToken(params = {}){
+     return jwt.sign(params, authConfig.secret,{
+        expiresIn: 86400,
+    } )
+}
+
+
 
 router.post('/register', async (req, res) => {
     const {email} = req.body;
@@ -25,7 +32,10 @@ router.post('/register', async (req, res) => {
         
         user.password = undefined;
 
-        return res.send({user});
+        return res.send({
+            user,
+            token: generateToken({id: user.id}),
+        });
     } catch (err) {
         return res.status(400).send({ error: 'falha no registro'});
     }
@@ -45,12 +55,10 @@ router.post('/authenticate', async (req, res) => {
 
         user.password = undefined;
 
-        const token = jwt.sign({ id: user.id }, authConfig.secret,{
-            expiresIn: 86400,
-        } )
-
-
-        res.send({ user, token });
+        res.send({ 
+            user,
+            token: generateToken({id: user.id}),
+        });
 });
 
 

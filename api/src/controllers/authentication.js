bba1aedebbcf78553dd2f21264288b78ecfa7f8f
lucasilmor <77138273+lucasilmor@users.noreply.github.com>
2021-03-20 02:46:11 -0300

    token

--- api/src/controllers/authentication.js
+++ api/src/controllers/authentication.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const authConfig = require('../config/auth');
+
 const User = require('../models/User');
+
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+
 
 const router = express.Router();
 
@@ -21,6 +29,28 @@ router.post('/register', async (req, res) => {
     } catch (err) {
         return res.status(400).send({ error: 'falha no registro'});
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;   
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).send({ error: 'usuário não encontrado' });
+    
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error: 'senha invalida' });
+
+        user.password = undefined;
+
+        const token = jwt.sign({ id: user.id }, authConfig.secret,{
+            expiresIn: 86400,
+        } )
+
+
+        res.send({ user, token });
 });
 
 
--- api/src/database/index.js
+++ api/src/database/index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/noderest', {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 mongoose.Promise = global.Promise;

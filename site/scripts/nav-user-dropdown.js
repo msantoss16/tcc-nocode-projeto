849ad6c4d88b0021a47deb9cf95e84365d5eb1f8
Torrecilha <torrecilha13@gmail.com>
2021-04-23 02:24:34 -0300

    tela chose-models pronta

---
+++ site/scripts/nav-user-dropdown.js
@@ -0,0 +1,8 @@
+function showDropdown() {
+    let click = document.getElementById("dropdown-content");
+    click.classList.toggle("active");
+}
+
+function myProfile(){
+    window.location.replace("/my-profile.html")
+}

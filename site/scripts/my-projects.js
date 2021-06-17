4f2c13fa63e711200ee9979423e8b9c215b3cd36
Torrrecilha <torrecilha13@gmail.com>
2021-06-16 21:48:05 -0300

    icone voltar chose-way / arrumando disposição de elementos na tela my-projects / modal de exclusão de projeto em my-project

--- site/scripts/my-projects.js
+++ site/scripts/my-projects.js
@@ -1,3 +1,28 @@
 function newProject(){
-    window.location.replace("/site/chose-creation-way.html")
+    window.location.href("/site/chose-creation-way.html")
 }
+
+function showDeleteModal(){
+    var deleteModal = document.getElementById("delete-project-modal");
+    deleteModal.style.opacity = "1";
+    deleteModal.style.top = "1rem";
+}
+
+function deleteProject(){
+    var deleteModal = document.getElementById("delete-project-modal");
+    var deletedModal = document.getElementById("deleted-project-modal");
+    deleteModal.style.opacity = "0";
+    deleteModal.style.top = "-8rem"
+    deletedModal.style.opacity = "1";
+    deletedModal.style.top = "1rem";
+    setTimeout(function(){
+        deletedModal.style.opacity = "0";
+        deletedModal.style.top = "-6rem";
+    }, 3000);
+};
+
+function cancelDelete(){
+    var deleteModal = document.getElementById("delete-project-modal");
+    deleteModal.style.opacity = "0";
+    deleteModal.style.top = "-8rem"
+};

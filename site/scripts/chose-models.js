a8ff52d29fb96dc717221ec857ed01547257ed9a
Torrrecilha <torrecilha13@gmail.com>
2021-06-20 23:20:34 -0300

    chose-models / novo logo adicionado

---
+++ site/scripts/chose-models.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  var zindex = 10;
+  
+  $("div.model").click(function(e){
+    e.preventDefault();
+
+    var isShowing = false;
+
+    if ($(this).hasClass("show")) {
+      isShowing = true
+    }
+
+    if ($("div.models-container").hasClass("showing")) {
+      $("div.model.show")
+        .removeClass("show");
+
+      if (isShowing) {
+        $("div.models-container")
+          .removeClass("showing");
+      } else {
+        $(this)
+          .css({zIndex: zindex})
+          .addClass("show");
+      }
+
+      zindex++;
+
+    } else {
+      $("div.models-container")
+        .addClass("showing");
+      $(this)
+        .css({zIndex:zindex})
+        .addClass("show");
+
+      zindex++;
+    }
+    
+  });
+});
